a2bcff23f6eb1b2b2a983c8e7495e1959bac8d38
David Konsumer <konsumer@jetboystudio.com>
2013-05-20 12:19:50 -0700

    starting work on new ideas

--- firmware/dkart/dkart.ino
+++ firmware/dkart/dkart.ino
@@ -2,56 +2,100 @@
 //  WARNING  //
 ///////////////
 // this isn't done, yet.
-// this is an untested logo-emulator, for initial testing
 
-// current gameboy-requested address
-unsigned int address = 0;
+#include <SD.h>
 
-// The nintendo logo
-const byte nintendoLogo[48] = {0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00,
-0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08,
-0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD,
-0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC,
-0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E};
+// contains PROGMEM for dkart in rom[]
+// eventually, this might be on the SD
+// maybe not, to remove need for SDtoSROM() bootstrap time
+#include "dkart.h"
+
+/*
+
+Hardware Connections:
+
+Teensy 2++
+
+for i2c EEPROM:
+SCL   - PD0
+SDA   - PD1
+address pins 1&2 on EEPROM tied to GND
+
+for interrupts:
+WR     - PE6 (arduino 18)
+CS     - PE7 (arduino 19)
+
+for SDCard
+SS     - PB0 (arduino 20)
+SCLK   - PB1 (arduino 21)
+MOSI   - PB2 (arduino 22)
+MISO   - PB3 (arduino 23)
+
+Gameboy Data/Address
+D0-D7  - PF0-PF7 (arduino 38-45)
+A0-A7  - PA0-PA7 (inside pins: arduino 28-35)
+A8-A15 - PC0-PC7 (arduino 10-17)
+
+*/
+
+File dataFile;
+
+// read WR & CS when either of them change.
+// also read address lines & read/write data lines
+void intReadWriteCS(){
+  word address = word(PINC, PINA);
+  
+  // Data read/write based on status of WR (active-low)
+  if (digitalRead(18)){ // WR HIGH
+    DDRF = 0xFF; // Data: OUTPUT
+    // TODO: use CS pin to choose bank
+    // TODO: this is dkart only, for now
+    if (address < 12080){
+      PORTF = (char)pgm_read_word(&(rom[address]));
+    }else{
+      PORTF = 0xFF;
+    }
+  }else{
+    DDRF = 0x00; // Data: INPUT
+  }
+}
+
+// read SD copy of ROM to SROM
+void SDtoSROM(char* rom){
+  dataFile = SD.open(rom);
+  if (dataFile) {
+    while (dataFile.available()) {
+      
+    }
+  }
+  dataFile.close();
+  // TODO: check for game RAM file, write it to SROM, too
+}
 
 void setup(){
-  // gameboy A-pins are read on PORTD & PORTA
-  DDRD = 0;
-  DDRA = 0;
+  // Address: INPUT
+  DDRA = 0x00;
+  DDRC = 0x00;
   
-  // gameboy RST, CS, WR, RD pins in read-mode, setup SD SPI
-  DDRB = B11010000;
+  // Data: OUTPUT
+  DDRF = 0xFF;
   
-  // on boot gameboy D-pins are write on PORTC
-  DDRC = 0xf;
+  // TODO:
+  // check if current ROM is loaded in SROM
+  // if no, load it from SD
+  // if yes, serve it instead of dkart
+  
+  // read SD of current-ROM to SROM
+  // normally, this would be called on game ROM select, and merely checked in setup() for dkart service
+  SDtoSROM("mario.gb");
+  
+  // read WR & CS when either of them change.
+  // also read address lines & read/write data lines
+  pinMode(18, INPUT);
+  pinMode(19, INPUT);
+  attachInterrupt(6, intReadWriteCS, CHANGE);
+  attachInterrupt(7, intReadWriteCS, CHANGE);
 }
 
 void loop(){
-  address = PIND + ( PINA << 8 );
-  // TODO: check CS on PB5 to make higher addresses
-  
-  // wait for noise to finish with delay
-  // later, this time could be used for SD-stuff
-  while(address == 0){
-    address = PIND + ( PINA << 8 );
-    if (address > 0) { // Hit the noise
-      delay(50);
-    }
-  }
-  
-  boolean readyToOutput = false;
-  
-  PORTC = nintendoLogo[0];
-  while (address != 68) { // Indicates the end of reading the header
-    address = PIND + ( PINA << 8 );
-    if (address == 4) { readyToOutput = true; }
-    if (address >= 4 && address <= 51 && readyToOutput == true) {
-      PORTC = nintendoLogo[address-3];
-    }
-  }
-  
-  // turn off output
-  PORTC = 0;
 }
-
-
