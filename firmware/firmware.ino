b5e6c5afad554e2f96ee95f75821289738008ad6
David Konsumer <konsumer@jetboystudio.com>
2020-03-09 20:16:25 -0700

    new ideas for firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,116 +1,83 @@
-/**
-This will eventually be the arduino-based firmware for dkart
-I will keep a running-tally of what it can currently do & how to hook it up, here
+/*
+HARDWARE:
 
-I am using this:
-https://www.banggood.com/Geekcreit-ESP32-WiFibluetooth-Development-Board-Ultra-Low-Power-Consumption-Dual-Cores-Unsoldered-p-1214159.html
+ESP32: https://www.banggood.com/Geekcreit-ESP32-WiFibluetooth-Development-Board-Ultra-Low-Power-Consumption-Dual-Cores-Unsoldered-p-1214159.html
+SDCard hooked to P5 (SS), P18 (SCK), P19 (MISO), P23 (MOSI)
 
-basic boot up, read SDCard, get count & list of filenames (paged, char[15][20]) 
-
+Currently this just builds a dkart.sav from the current file-list
 */
 
 #include "SdFat.h"
-#include "sdios.h"
 
-// SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
-// 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
-#define SD_FAT_TYPE 3
+// this is maybe a bit specifc to my hardware, see SdFat examples to tune to yours, if needed
+#define SD_CONFIG SdSpiConfig(SS, DEDICATED_SPI, SD_SCK_MHZ(16))
 
-// SDCARD_SS_PIN is defined for the built-in SD on some boards.
-#ifndef SDCARD_SS_PIN
-  const uint8_t SD_CS_PIN = SS;
-#else  // SDCARD_SS_PIN
-  const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
-#endif  // SDCARD_SS_PIN
+// This figures out correct filesystem (EX/FAT16/32)
+SdFs sd;
+FsFile file;
+FsFile root;
+FsFile sav;
 
-// Try to select the best SD card configuration.
-#if HAS_SDIO_CLASS
-  #define SD_CONFIG SdioConfig(FIFO_SDIO)
-#elif ENABLE_DEDICATED_SPI
-  #define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(50))
-#else  // HAS_SDIO_CLASS
-  #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SD_SCK_MHZ(16))
-#endif  // HAS_SDIO_CLASS
+// TODO: put dkart ROM data in PROGMEN? https://forum.arduino.cc/index.php?topic=530095.0 or check for it on SD
+// TODO: detect if I need to upper-case filenames in checks/opens
+// TODO: blink on error
+// TODO: use GB-CLK interrupt to sleep/read
 
-///////////////////////////////////////////////////////////////////////////
-
-#if SD_FAT_TYPE == 0
-  SdFat sd;
-  File file;
-  File root;
-#elif SD_FAT_TYPE == 1
-  SdFat32 sd;
-  File32 file;
-  File32 root;
-#elif SD_FAT_TYPE == 2
-  SdExFat sd;
-  ExFile file;
-  ExFile root;
-#elif SD_FAT_TYPE == 3
-  SdFs sd;
-  FsFile file;
-  FsFile root;
-#endif  // SD_FAT_TYPE
-
-ArduinoOutStream cout(Serial);
-
-// Store error strings in flash to save RAM.
 #define error(s) sd.errorHalt(&Serial, F(s))
 
-// TODO: put dkart ROM data in PROGMEN? https://forum.arduino.cc/index.php?topic=530095.0
-
 unsigned long romCount = 0;
-char* romPage[15];
-unsigned int currentPage = 0;
-unsigned long pageStart;
-unsigned long pageEnd;
-
-// this gets totalCount & current page
-// TODO: write this to SRAM file (dkart.sav) so I can use generic routines
-void getROMs() {
-  if (!root.open("/")) {
-    error("open root");
-  }
-  
-  romCount = 0;
-  pageStart = currentPage * 15;
-  pageEnd = pageStart + 15;
-
-  while (file.openNext(&root, O_RDONLY)) {
-    // TODO: handle sub-dirs
-    // TODO: check extension
-    if (!file.isHidden() && !file.isSubDir()) {
-      romCount++;
-      if (romCount >= pageStart && romCount <= pageEnd) {
-        memcpy(romPage[ romCount - pageStart ], file.name(), 20);
-      }
-    }
-    file.close();
-  }
-}
-
+char currentRom[255] = "dkart.gb";
+char currentSav[255] = "dkart.sav";
 
 void setup() {
-  // TODO: initialize interrupt for clock to R/W from SD
-  // TODO: use EEPROM to store current ROM
-  // TODO: send custom startup logo on second read (maybe instead of spalsh screen)
+  Serial.begin(115200);
   
-  Serial.begin(9600);
-  while (!Serial) {
-    SysCall::yield();
-  }
-  delay(1000);
-  cout << F("Type any character to start\n");
-  while (!Serial.available()) {
-    SysCall::yield();
-  }
-  
+  Serial.println("Hi.");
+
   if (!sd.begin(SD_CONFIG)) {
     sd.initErrorHalt(&Serial);
   }
-  getROMs();
-  cout << "ROMS: " << romCount << "\n";
+
+  // read file-list into dkart.sav
+  if (!root.open("/")) {
+    error("open root");
+  }
+
+  // TODO: lookup currentRom from EEPROM/SDCard
+  // TODO: derive currentSav from currentRom
+  
+  if (!sav.open(currentSav, O_CREAT|O_WRITE)) {
+    error("creating SAV");
+  }
+  
+  romCount = 0;
+  sav.seek(4);
+  while (file.openNext(&root, O_RDONLY)) {
+     char fname[21];
+     file.getName(fname, 21);
+     Serial.println(fname);
+     if (!file.isHidden() && !file.isSubDir() && strcmp(".gb", &fname[ strlen(fname) - 3 ]) == 0) {
+      // TODO: get name from header instead of filename
+      sav.write(fname, 20);
+      romCount++;
+     }
+     file.close();
+  }
+  
+  sav.seek(0);
+  // turn romCount into 4-bytes at beginning of file
+  byte rc[4] = {
+    (romCount >> 24) & 0xFF,
+    (romCount >> 16) & 0xFF,
+    (romCount >> 8) & 0xFF,
+    romCount & 0xFF
+  };
+  sav.write(rc, 4);
+  sav.close();
+  
+  Serial.write("ROMS Found: ");
+  Serial.println(romCount);
 }
 
-void loop() {  
+void loop() {
 }
