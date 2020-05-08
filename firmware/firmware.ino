9bf6dc9bf03dc3384475933851717c593325bf45
David Konsumer <konsumer@jetboystudio.com>
2020-05-08 06:46:23 -0700

    hardware notes

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,6 +10,58 @@ Currently this just builds a dkart.sav from the current file-list
 // use https://github.com/greiman/SdFat-beta
 #include "SdFat.h"
 
+// this defines the hardware
+
+// SD on HSPI - http://www.ayarafun.com/wp-content/uploads/2016/11/esp32-pinout.png
+// MISO - 12
+// MOSI - 13
+// SCK - 14
+// SS - 15
+
+// https://dhole.github.io/media/gameboy_stm32f4/gb_cartridge_pins.jpg
+
+// it's maxed-out, so no i2c or serial or wifi
+// probly should use a shift-in (like 74F676) to handle GB address
+// with i2s sort of like this:
+// https://github.com/Crypter/CryptoIO
+// https://github.com/lhartmann/esp8266_reprap
+// https://esp32.com/viewtopic.php?f=17&t=3188
+// i2s allows very fast low-cpu serial
+
+#define GB_A0  GPIO36 
+#define GB_A1  GPIO39
+#define GB_A2  GPIO34
+#define GB_A3  GPIO35
+#define GB_A4  GPIO32
+#define GB_A5  GPIO33
+#define GB_A6  GPIO25
+#define GB_A7  GPIO26
+#define GB_A8  GPIO27
+#define GB_A9  GPIO09
+#define GB_A10 GPIO10
+#define GB_A11 GPIO11
+#define GB_A12 GPIO23 
+#define GB_A13 GPIO22
+#define GB_A14 GPIO01
+#define GB_A15 GPIO03
+
+#define GB_D0  GPIO21
+#define GB_D1  GPIO19 
+#define GB_D2  GPIO18
+#define GB_D3  GPIO05
+#define GB_D4  GPIO17
+#define GB_D5  GPIO16 
+#define GB_D6  GPIO04
+#define GB_D7  GPIO00
+
+#define GB_WR  GPIO02
+#define GB_RD  GPIO08
+#define GB_CLK GPIO07
+#define GB_CS  GPIO06
+
+// #define GB_RST 
+#define GB_AUD ADC12 // GPIO02
+
 // this is maybe a bit specifc to my hardware, see SdFat examples to tune to yours, if needed
 #define SD_CONFIG SdSpiConfig(SS, DEDICATED_SPI, SD_SCK_MHZ(16))
 
